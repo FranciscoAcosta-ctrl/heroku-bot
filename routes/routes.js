35f1dce0a8791748e44a48b110f49d2c671dc27c
unknown <franciscow4tcher@gmail.com>
2022-02-25 17:22:24 -0600

    data

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const Product=require("../Models/")
 
   router.get("/compra", (req, res) => {
     return res.render("products");
